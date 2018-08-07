69f18b71395ebed1898ac324ab3b6d3fb250c8cf
ayugrover <32064732+ayugrover@users.noreply.github.com>
2018-08-07 12:00:25 +0530

    Add files via upload

---
+++ explicit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class explicit
+{
+	public static void main(String args[]){
+	Scanner sc=new Scanner(System.in);
+		double x = sc.nextDouble();
+        int y = (int) x;
+        System.out.println("The value of y is: "+ y);
+	}
+}
