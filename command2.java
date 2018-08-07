8abcd81b6aa24268ddcd641c2cd16742cc328f3a
ayugrover <32064732+ayugrover@users.noreply.github.com>
2018-08-07 11:59:31 +0530

    Add files via upload

---
+++ command2.java
@@ -0,0 +1,11 @@
+class command2
+{
+	public static void main(String args[]){
+		System.out.println("The required string is : ");
+			for(int i=0;i<args.length;i++)
+			{
+				System.out.print(args[i]);
+			}
+		}
+	
+}
