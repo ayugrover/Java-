724b83c43623d51d45a67ed7a239551fabe2f85d
ayugrover <32064732+ayugrover@users.noreply.github.com>
2018-08-07 12:01:36 +0530

    Add files via upload

---
+++ implicit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class implicit{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int x = sc.nextInt();
+        double y = x;
+        System.out.println("The value of y is: "+ y);	
+	}
+}
