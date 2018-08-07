e6a96ea63fe1ee5a07505b4df265fada3d7e3920
ayugrover <32064732+ayugrover@users.noreply.github.com>
2018-08-07 12:02:39 +0530

    Add files via upload

---
+++ buffer.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+class buffer{  
+public static void main(String args[])throws java.lang.Exception{  
+BufferedReader sb1 =new BufferedReader(new InputStreamReader(System.in));
+ String str = sb1.readLine();
+System.out.println(str);
+}  
+}  
